3aeaff7918d6f7586dacf4ccc0bd30f81278e93a
LorenzoZanone <127969779+LorenzoZanone@users.noreply.github.com>
2023-07-12 11:38:07 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
+//falta cohesion malisimo
 import java.util.ArrayList;
 import javax.swing.JOptionPane;
 
