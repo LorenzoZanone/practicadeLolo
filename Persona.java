2b3d71119ff2875c5d355ea012e1039e005d0007
LorenzoZanone <127969779+LorenzoZanone@users.noreply.github.com>
2023-07-12 11:43:47 -0300

    Update Persona.java

--- Persona.java
+++ Persona.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
+//no le preguntas con que genero se identifica y sus pronombres? malisimo
 public abstract class Persona {
 
     int dni;
